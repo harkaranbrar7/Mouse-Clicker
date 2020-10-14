664bf2bde3743059a6fc6e3a85d1403c48b65930
harkaranbrar7 <harkaranbrar7@gmail.com>
2020-10-13 20:30:10 -0700

    fixes

---
+++ Mouseclicker.py
@@ -0,0 +1,25 @@
+from pynput.mouse import Button, Controller
+import threading
+
+import time
+
+
+def clickIt():
+    threading.Timer(30.0, clickIt).start() # called every minute
+    mouse = Controller()
+    print("Mouse Position: ",mouse.position)
+    mouse.position = (3370,750)
+    # mouse.click(Button.left, 2)
+    mouse.click(Button.right,1)
+    print("Right Button Clicked")
+    time.sleep(2)
+    mouse.position = (3395,840)
+    print("left Button Clicked")
+    mouse.click(Button.left,1)
+    print("Refreshed")
+    print('-'*50) 
+    
+
+clickIt()
+
+
